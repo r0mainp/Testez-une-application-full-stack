1b17feb82be0798e7a187841d669a9d9fd94e096
RomainP <romainportier7@gmail.com>
2024-07-29 15:42:24 +0200

    add unit tests for email and password inputs

--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -33,8 +33,104 @@ describe('RegisterComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+  
+  describe('Register Unit Test suite', ()=> {
+    
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    it('should indicate an error when email is empty', () => {
+      component.form.controls['email'].setValue('');
+
+      component.form.controls['email'].markAsTouched();
+      component.form.controls['email'].updateValueAndValidity();
+
+      fixture.detectChanges();
+
+      const emailInput = fixture.debugElement.nativeElement.querySelector('input[formControlName="email"]');
+
+      expect(emailInput.classList).toContain('ng-invalid');
+    });
+
+    it('should indicate an error when email is invalid', () => {
+
+      component.form.controls['email'].setValue('invalidEmail');
+
+      component.form.controls['email'].markAsTouched();
+      component.form.controls['email'].updateValueAndValidity();
+
+      fixture.detectChanges(); 
+
+      const emailInput = fixture.debugElement.nativeElement.querySelector('input[formControlName="email"]');
+
+      expect(emailInput.classList).toContain('ng-invalid');
+    });
+
+    it('should indicate an error when password is empty', () => {
+      component.form.controls['password'].setValue('');
+
+      component.form.controls['password'].markAsTouched();
+      component.form.controls['password'].updateValueAndValidity();
+
+      fixture.detectChanges();
+
+      const passwordInput = fixture.debugElement.nativeElement.querySelector('input[formControlName="password"]');
+
+      expect(passwordInput.classList).toContain('ng-invalid');
+    });
+
+    it('should indicate an error when password length < 3', () => {
+      component.form.controls['password'].setValue('te');
+
+      component.form.controls['password'].markAsTouched();
+      component.form.controls['password'].updateValueAndValidity();
+
+      fixture.detectChanges();
+
+      const passwordInput = fixture.debugElement.nativeElement.querySelector('input[formControlName="password"]');
+
+      expect(passwordInput.classList).toContain('ng-invalid');
+    });
+
+    it('should indicate an error when password length > 40', () => {
+      component.form.controls['password'].setValue('This is a string with exactly forty-one chars.');
+
+      component.form.controls['password'].markAsTouched();
+      component.form.controls['password'].updateValueAndValidity();
+
+      fixture.detectChanges();
+
+      const passwordInput = fixture.debugElement.nativeElement.querySelector('input[formControlName="password"]');
+
+      expect(passwordInput.classList).toContain('ng-invalid');
+    });
+
+    it('should indicate no error when email is valid', () => {
+      component.form.controls['email'].setValue('test@test.com');
+ 
+      component.form.controls['email'].markAsTouched();
+      component.form.controls['email'].updateValueAndValidity();
+
+      fixture.detectChanges();
+
+      const emailInput = fixture.debugElement.nativeElement.querySelector('input[formControlName="email"]');
+
+      expect(emailInput.classList).toContain('ng-valid');
+    })
+
+    it('should indicate no error when password is valid', () => {
+
+      component.form.controls['password'].setValue('test');
+
+      component.form.controls['password'].markAsTouched();
+      component.form.controls['password'].updateValueAndValidity();
+
+      fixture.detectChanges();
+
+      const passwordInput = fixture.debugElement.nativeElement.querySelector('input[formControlName="password"]');
+
+      expect(passwordInput.classList).toContain('ng-valid');
+    })
   });
 });
