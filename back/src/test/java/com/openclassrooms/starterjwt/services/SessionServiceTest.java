e2df197a6848e98e06e1077a17860482835c4de6
RomainP <romainportier7@gmail.com>
2024-08-01 15:28:53 +0200

    add session participate unit test

--- back/src/test/java/com/openclassrooms/starterjwt/services/SessionServiceTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/services/SessionServiceTest.java
@@ -2,6 +2,7 @@ package com.openclassrooms.starterjwt.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doNothing;
@@ -20,12 +21,17 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import com.openclassrooms.starterjwt.models.Session;
+import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.repository.SessionRepository;
+import com.openclassrooms.starterjwt.repository.UserRepository;
 
 public class SessionServiceTest {
 
     @Mock
     private SessionRepository sessionRepository;
+
+    @Mock
+    private UserRepository userRepository;
 
     @InjectMocks
     private SessionService sessionService;
@@ -77,5 +83,29 @@ public class SessionServiceTest {
         sessionService.delete(1L);
         verify(sessionRepository, times(1)).deleteById(1L);
     }
+
+    @Test
+    public void testParticipate(){
+        Session session = new Session();
+        session.setUsers(new ArrayList<>());
+
+        User user = new User();
+        user.setId(1L);
+
+        when(sessionRepository.findById(1L)).thenReturn(Optional.of(session));
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+        when(sessionRepository.save(any(Session.class))).thenReturn(session);
+
+        sessionService.participate(1L, 1L);
+
+        assertTrue(session.getUsers().contains(user));
+
+        verify(sessionRepository, times(1)).findById(1L);
+        verify(userRepository, times(1)).findById(1L);
+        verify(sessionRepository, times(1)).save(session);
+
+    }
+
+    // TODO test exceptions
 
 }
