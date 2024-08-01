f052b30a42e5506fd10a93db93dca7be1f17629e
RomainP <romainportier7@gmail.com>
2024-08-01 15:08:09 +0200

    remove useless imports

--- back/src/test/java/com/openclassrooms/starterjwt/services/UserServiceTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/services/UserServiceTest.java
@@ -7,8 +7,6 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
