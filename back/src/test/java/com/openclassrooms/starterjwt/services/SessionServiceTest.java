b5c09f5301df8268a02ebcaf89059862a269d518
RomainP <romainportier7@gmail.com>
2024-08-01 12:57:22 +0200

    add session get by id unit test

--- back/src/test/java/com/openclassrooms/starterjwt/services/SessionServiceTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/services/SessionServiceTest.java
@@ -2,12 +2,14 @@ package com.openclassrooms.starterjwt.services;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -39,4 +41,14 @@ public class SessionServiceTest {
         assertNotNull(result);
         verify(sessionRepository, times(1)).findAll();
     }
+
+    @Test
+    public void testGetById() {
+        Session session = new Session();
+        when(sessionRepository.findById(anyLong())).thenReturn(Optional.of(session));
+        Session result = sessionService.getById(1L);
+        assertNotNull(result);
+        verify(sessionRepository, times(1)).findById(1L);
+    }
+
 }
