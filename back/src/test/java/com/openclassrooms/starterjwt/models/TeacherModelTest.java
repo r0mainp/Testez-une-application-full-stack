106f700c248cc7a2ac2d313769749d5df722d5ab
RomainP <romainportier7@gmail.com>
2024-08-01 21:10:51 +0200

    add Teacher model builder unit test

---
+++ back/src/test/java/com/openclassrooms/starterjwt/models/TeacherModelTest.java
@@ -0,0 +1,48 @@
+package com.openclassrooms.starterjwt.models;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+
+public class TeacherModelTest {
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+    
+    @Test
+    public void testTeacherBuilder() {
+        LocalDateTime now = LocalDateTime.now();
+
+        Teacher.TeacherBuilder builder = Teacher.builder()
+                .id(1L)
+                .lastName("Portier")
+                .firstName("Romain")
+                .createdAt(now)
+                .updatedAt(now);
+
+        Teacher teacher = builder.build();
+
+        assertNotNull(teacher);
+        assertEquals(1L, teacher.getId());
+        assertEquals("Portier", teacher.getLastName());
+        assertEquals("Romain", teacher.getFirstName());
+        assertEquals(now, teacher.getCreatedAt());
+        assertEquals(now, teacher.getUpdatedAt());
+
+        String teacherString = builder.toString();
+        assertNotNull(teacherString);
+        assertTrue(teacherString.contains("Teacher.TeacherBuilder(id=1"));
+        assertTrue(teacherString.contains("lastName=Portier"));
+        assertTrue(teacherString.contains("firstName=Romain"));
+        assertTrue(teacherString.contains("createdAt="));
+        assertTrue(teacherString.contains("updatedAt="));
+    }
+}
