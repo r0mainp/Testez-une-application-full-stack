9cb7fc342634d7dc3b36393f7af5944247df4f64
RomainP <romainportier7@gmail.com>
2024-08-02 20:52:55 +0200

    add Session controller create integration test

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/SessionControllerIntegrationTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/SessionControllerIntegrationTest.java
@@ -20,9 +20,12 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.openclassrooms.starterjwt.dto.SessionDto;
 import com.openclassrooms.starterjwt.mapper.SessionMapper;
 import com.openclassrooms.starterjwt.models.Session;
+import com.openclassrooms.starterjwt.models.Teacher;
+import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.services.SessionService;
 
 @SpringBootTest
@@ -38,8 +41,8 @@ public class SessionControllerIntegrationTest {
     @MockBean
     private SessionMapper sessionMapper;
 
-    // @Autowired
-    // private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     private Session session;
     private SessionDto sessionDto;
@@ -48,13 +51,28 @@ public class SessionControllerIntegrationTest {
     public void setUp() {
         MockitoAnnotations.openMocks(this);
 
+        // User sessionOwner = User.builder()
+        //     .id(1L)
+        //     .email("test@test.com")
+        //     .firstName("John")
+        //     .lastName("Doe")
+        //     .password("password")
+        //     .admin(false)
+        //     .build();
+        
+        Teacher teacher = Teacher.builder()
+            .id(1L)
+            .firstName("Someone")
+            .lastName("Else")
+            .build();
+
         session = new Session();
         session.setId(1L);
         session.setName("Test Session");
         session.setDate(new Date());
         session.setDescription("Test Description");
-        session.setTeacher(null);  // Set to a proper Teacher instance if needed
-        session.setUsers(null);    // Set to a proper list of User instances if needed
+        session.setTeacher(teacher);
+        session.setUsers(null);
         session.setCreatedAt(LocalDateTime.now());
         session.setUpdatedAt(LocalDateTime.now());
 
@@ -63,8 +81,8 @@ public class SessionControllerIntegrationTest {
         sessionDto.setName("Test Session");
         sessionDto.setDate(new Date());
         sessionDto.setDescription("Test Description");
-        sessionDto.setTeacher_id(null);  // Set to a proper teacher ID if needed
-        sessionDto.setUsers(null);       // Set to a proper list of user IDs if needed
+        sessionDto.setTeacher_id(1L);
+        sessionDto.setUsers(null);
         sessionDto.setCreatedAt(LocalDateTime.now());
         sessionDto.setUpdatedAt(LocalDateTime.now());
     }
@@ -86,7 +104,7 @@ public class SessionControllerIntegrationTest {
                 .andExpect(jsonPath("$[0].name").value("Test Session"))
                 .andExpect(jsonPath("$[0].description").value("Test Description"))
                 .andExpect(jsonPath("$[0].date").exists())
-                .andExpect(jsonPath("$[0].teacher_id").doesNotExist())
+                .andExpect(jsonPath("$[0].teacher_id").value(1L))
                 .andExpect(jsonPath("$[0].users").doesNotExist());
     }
 
@@ -103,7 +121,26 @@ public class SessionControllerIntegrationTest {
                 .andExpect(jsonPath("$.name").value("Test Session"))
                 .andExpect(jsonPath("$.description").value("Test Description"))
                 .andExpect(jsonPath("$.date").exists())
-                .andExpect(jsonPath("$.teacher_id").doesNotExist())
+                .andExpect(jsonPath("$.teacher_id").value(1L))
+                .andExpect(jsonPath("$.users").doesNotExist());
+    }
+
+    @Test
+    @WithMockUser
+    public void testCreate() throws Exception {
+        when(sessionMapper.toEntity(sessionDto)).thenReturn(session);
+        when(sessionService.create(session)).thenReturn(session);
+        when(sessionMapper.toDto(session)).thenReturn(sessionDto);
+
+        mockMvc.perform(post("/api/session")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(sessionDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.name").value("Test Session"))
+                .andExpect(jsonPath("$.description").value("Test Description"))
+                .andExpect(jsonPath("$.date").exists())
+                .andExpect(jsonPath("$.teacher_id").value(1L))
                 .andExpect(jsonPath("$.users").doesNotExist());
     }
 }
