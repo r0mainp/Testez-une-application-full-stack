fe7cd6d183ad28562e634306e2b3ac3d144e69fd
RomainP <romainportier7@gmail.com>
2024-08-02 13:46:08 +0200

    add session mapper toDto full unit test

--- back/src/test/java/com/openclassrooms/starterjwt/mapper/SessionMapperTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/mapper/SessionMapperTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -62,5 +62,58 @@ public class SessionMapperTest {
         assertEquals("Description", session.getDescription());
         assertNotNull(session.getTeacher());
         assertEquals(2, session.getUsers().size());
+    }
+
+    @Test
+    public void testToDtoList(){
+        // Create first session
+        Session session1 = new Session();
+        session1.setDescription("Description session 1");
+
+        Teacher teacher1 = new Teacher();
+        teacher1.setId(1L);
+        session1.setTeacher(teacher1);
+
+        User user1 = new User();
+        user1.setId(1L);
+        User user2 = new User();
+        user2.setId(2L);
+
+        session1.setUsers(Arrays.asList(user1, user2));
+
+        // Create second Session
+        Session session2 = new Session();
+        session2.setDescription("Description session 2");
+
+        Teacher teacher2 = new Teacher();
+        teacher2.setId(2L);
+        session2.setTeacher(teacher2);
+
+        User user3 = new User();
+        user3.setId(3L);
+        User user4 = new User();
+        user4.setId(4L);
+        session2.setUsers(Arrays.asList(user3, user4));
+
+        //Make a list with sessions
+        List<Session> sessions = Arrays.asList(session1, session2);
+
+        // Call toDto with
+        List<SessionDto> sessionDtos = sessionMapper.toDto(sessions);
+
+        //Do the checks
+        SessionDto dto1 = sessionDtos.get(0);
+        assertEquals("Description session 1", dto1.getDescription());
+        assertEquals(1L, dto1.getTeacher_id());
+        assertEquals(2, dto1.getUsers().size());
+        assertTrue(dto1.getUsers().contains(1L));
+        assertTrue(dto1.getUsers().contains(2L));
+
+        SessionDto dto2 = sessionDtos.get(1);
+        assertEquals("Description session 2", dto2.getDescription());
+        assertEquals(2L, dto2.getTeacher_id());
+        assertEquals(2, dto2.getUsers().size());
+        assertTrue(dto2.getUsers().contains(3L));
+        assertTrue(dto2.getUsers().contains(4L));
     }
 }
